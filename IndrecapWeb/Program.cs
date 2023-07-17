b843b3f0995dc8e32a440f28912a9ff2a2405431
Slash <wwwroot@mail.ksu.edu.tw>
2023-07-17 09:39:16 +0800

    Init

---
+++ IndrecapWeb/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace IndrecapWeb
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      System.Console.WriteLine("This project is for sharing internet pages.");
+      System.Console.Read();
+    }
+  }
+}
